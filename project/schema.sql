35805c6d85cbc7ae1b6d3575b281fb27f19e6012
Aaditya Bansal <adiaturb@gmail.com>
2023-11-14 00:03:56 +0530

    updated schema with tables for articles

--- project/schema.sql
+++ project/schema.sql
@@ -11,10 +11,17 @@ DROP TABLE IF EXISTS Related_content CASCADE;
 DROP TABLE IF EXISTS Related_web_search_results CASCADE;
 DROP TABLE IF EXISTS Related_question CASCADE;
 DROP TABLE IF EXISTS Related_video CASCADE;
-DROP TABLE IF EXISTS User_Tag CASCADE;
 DROP TABLE IF EXISTS Follow CASCADE;
 DROP TABLE IF EXISTS Post_Vote CASCADE;
-DROP TABLE IF EXISTS Question_Tag CASCADE;
+DROP TABLE IF EXISTS Quiz;
+DROP TABLE IF EXISTS Quiz_Question;
+DROP TABLE IF EXISTS Quiz_Question_User_Response;
+DROP TABLE IF EXISTS Quiz_Score_Card;
+DROP TABLE IF EXISTS Article;
+DROP TABLE IF EXISTS Article_Response;
+DROP TABLE IF EXISTS Article_Vote;
+DROP TABLE IF EXISTS Article_Response_Vote;
+DROP TABLE IF EXISTS Article_Response_Comment;
 
 CREATE TABLE IF NOT EXISTS App_user  ( 
     user_id serial PRIMARY KEY,
@@ -53,8 +60,6 @@ CREATE TABLE IF NOT EXISTS Question  (
     document_vectors tsvector
 ) INHERITS (Post);
 
-CREATE INDEX idx_question_doc_vec ON Question USING gin(document_vectors);
-
 CREATE TABLE IF NOT EXISTS Response  (
     response_id serial PRIMARY KEY,
     question_id integer NOT NULL,
@@ -64,11 +69,6 @@ CREATE TABLE IF NOT EXISTS Response  (
     CONSTRAINT fk_response_question
         FOREIGN KEY(question_id) 
         REFERENCES Question(question_id)
-        ON DELETE CASCADE,
-
-    CONSTRAINT fk_response_app_user
-        FOREIGN KEY(user_id) 
-        REFERENCES App_user(user_id)
         ON DELETE CASCADE
 ) INHERITS (Post);
 
@@ -179,23 +179,6 @@ CREATE TABLE IF NOT EXISTS Related_video  (
     player_embed_html text  NOT NULL
 ) INHERITS (Related_content);
 
-CREATE TABLE IF NOT EXISTS User_Tag  (
-    tag_name text,
-    user_id integer,
-
-    CONSTRAINT fk_user_tag_tag
-        FOREIGN KEY(tag_name) 
-        REFERENCES Tag(tag_name)
-        ON DELETE CASCADE,
-
-    CONSTRAINT fk_user_tag_app_user
-        FOREIGN KEY(user_id) 
-        REFERENCES App_user(user_id)
-        ON DELETE CASCADE,
-
-    PRIMARY KEY (tag_name, user_id)
-);
-
 CREATE TABLE IF NOT EXISTS Follow  (
     follower_user_id integer,
     followed_user_id integer,
@@ -234,29 +217,6 @@ CREATE TABLE IF NOT EXISTS Post_Vote (
         REFERENCES App_user(user_id)
         ON DELETE CASCADE
 );
-
-CREATE TABLE IF NOT EXISTS Question_Tag  (
-    tag_name text,
-    question_id integer,
-
-    CONSTRAINT fk_question_tag_tag
-        FOREIGN KEY(tag_name) 
-        REFERENCES Tag(tag_name)
-        ON DELETE CASCADE,
-
-    CONSTRAINT fk_question_tag_question
-        FOREIGN KEY(question_id) 
-        REFERENCES Question(question_id)
-        ON DELETE CASCADE,
-
-    PRIMARY KEY (tag_name, question_id)
-);
-
-INSERT INTO App_user 
-    (user_id, username, name, about) 
-VALUES 
-    (0, 'google_vertex_ai', 'Google Vertex AI', 'Generative AI chat bot by Google which provides quick first response to user questions.');
-
 
 CREATE TABLE Quiz (
     quiz_id serial PRIMARY KEY,
@@ -320,3 +280,97 @@ CREATE TABLE Quiz_Score_Card (
 
     PRIMARY KEY (quiz_id, user_id)  
 );
+
+CREATE TABLE Article (
+    article_id serial PRIMARY KEY,
+    title text NOT NULL,
+    contents text NOT NULL,
+    tags text[5],
+    thumbnail_url text,
+    vote_counter integer DEFAULT 0,
+    response_counter integer DEFAULT 0,
+    created_time timestamp DEFAULT CURRENT_TIMESTAMP,
+    user_id integer NOT NULL,
+
+    CONSTRAINT fk_article_app_user
+        FOREIGN KEY(user_id) 
+        REFERENCES App_user(user_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE Article_Response (
+    article_response_id serial PRIMARY KEY,
+    article_id integer NOT NULL,
+    contents text NOT NULL,
+    vote_counter integer DEFAULT 0,
+    response_counter integer DEFAULT 0,
+    created_time timestamp DEFAULT CURRENT_TIMESTAMP,
+    user_id integer NOT NULL,
+
+    CONSTRAINT fk_article_response_article
+        FOREIGN KEY(article_id) 
+        REFERENCES Article(article_id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_article_response_app_user
+        FOREIGN KEY(user_id) 
+        REFERENCES App_user(user_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE Article_Vote (
+    article_id integer,
+    user_id integer,
+    val integer DEFAULT 1,
+
+    CONSTRAINT fk_article_vote_article
+        FOREIGN KEY(article_id) 
+        REFERENCES Article(article_id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_post_vote_app_user
+        FOREIGN KEY(user_id) 
+        REFERENCES App_user(user_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE Article_Response_Vote (
+    article_response_id integer,
+    user_id integer,
+    val integer DEFAULT 1,
+
+    CONSTRAINT fk_article_vote_article_response
+        FOREIGN KEY(article_response_id) 
+        REFERENCES Article_Response(article_response_id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_post_vote_app_user
+        FOREIGN KEY(user_id) 
+        REFERENCES App_user(user_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE Article_Response_Comment (
+    article_response_comment_id serial PRIMARY KEY,
+    article_response_id integer, 
+    user_id integer, 
+    comment_text text NOT NULL, 
+    created_time timestamp DEFAULT CURRENT_TIMESTAMP,
+    
+    CONSTRAINT fk_article_response_comment_article_response
+        FOREIGN KEY(article_response_id) 
+        REFERENCES Article_Response(article_response_id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_article_response_comment_app_user
+        FOREIGN KEY(user_id) 
+        REFERENCES App_user(user_id)
+        ON DELETE CASCADE
+);
+
+CREATE INDEX idx_question_doc_vec ON Question USING gin(document_vectors);
+
+INSERT INTO App_user 
+    (user_id, username, name, about) 
+VALUES 
+    (0, 'google_vertex_ai', 'Google Vertex AI', 'Generative AI chat bot by Google which provides quick first response to user questions.');
